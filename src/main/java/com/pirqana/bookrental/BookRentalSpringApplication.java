f81a4ace9f1b899aa205d7b93fd222c852a84080
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-03-23 17:47:44 -0500

    Start project

---
+++ src/main/java/com/pirqana/bookrental/BookRentalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pirqana.bookrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRentalSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pirqana/bookrental/BookRentalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pirqana.bookrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
